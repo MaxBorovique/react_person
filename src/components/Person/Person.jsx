5953246c70598128e6ced175330de9ca30678441
Maksym <max.borovique@gmail.com>
2024-07-24 22:35:03 +0300

    solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, married, partner } = person;
+
+  let partnerInfo;
+
+  if (married) {
+    if (partner.gender === 'female') {
+      partnerInfo = `${partner.name} is my wife`;
+    } else {
+      partnerInfo = `${partner.name} is my husband`;
+    }
+  } else {
+    partnerInfo = 'I am not married';
+  }
+
+  return (
+    <div className="Person">
+      <div className="Person__name">My name is {name}</div>
+      {age && <div className="Person__age">I am {age}</div>}
+      <div className="Person__partner">{partnerInfo}</div>
+    </div>
+  );
+};
